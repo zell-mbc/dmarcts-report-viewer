64ddc06d4549c79aa7d72793d90bb7ca90817473
Dave <dave@techsneeze.com>
2016-04-13 11:01:28 -0700

    Added patch provided in Issue #3

--- dmarcts-report-viewer.php
+++ dmarcts-report-viewer.php
@@ -215,10 +215,16 @@ while($row = $query->fetch_assoc()) {
 	}
 }
 
+if(isset($_GET['report']) && is_numeric($_GET['report'])){
+	$reportid=$_GET['report'];
+}elseif(!isset($_GET['report'])){
+	$reportid=false;
+}else{
+	die('Invalid Report ID');
+}
 // Generate Page with report list and report data (if a report is selected).
 echo tmpl_page( ""
 	.tmpl_reportList($allowed_reports)
-	.tmpl_reportData( (isset($_GET["report"]) ? $_GET["report"] : false ), $allowed_reports )
+	.tmpl_reportData($reportid, $allowed_reports )
 );
-
 ?>
