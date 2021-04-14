d5d15e2e5647493d958d29fd1a974974bdb4f0be
jnew-gh <github@hazelden.ca>
2021-04-14 14:21:25 -0400

    Simplify SQL statement for Report Description
    Don't need such complex SQL statements for generating the Report Data table. This SQL statement simply needs to return the report metadata of one report (to generate the Report Description), not all the report records themselves.

--- dmarcts-report-viewer-report-data.php
+++ dmarcts-report-viewer-report-data.php
@@ -292,29 +292,12 @@ switch ($dmarc_select) {
 
 $sql = "
 SELECT
-	report.*,
-	sum(rptrecord.rcount) AS rcount,
-	MIN(rptrecord.dkimresult) AS dkimresult,
-	MIN(rptrecord.spfresult) AS spfresult
+	*
 FROM
 	report
-LEFT JOIN
-	(
-	SELECT
-		rcount,
-		COALESCE(dkimresult, 'neutral') AS dkimresult,
-		COALESCE(spfresult, 'neutral') AS spfresult,
-		serial
-	FROM
-		rptrecord
-	)
-	AS rptrecord
-ON
-	report.serial = rptrecord.serial
-WHERE report.serial = " . $mysqli->real_escape_string($reportid) . "
-GROUP BY serial
-ORDER BY mindate $sort, maxdate $sort , org";
-
+WHERE
+	serial = " . $mysqli->real_escape_string($reportid)
+;
 
 // Debug
 // echo "<br /><b>Data Report sql:</b> $sql<br />";
